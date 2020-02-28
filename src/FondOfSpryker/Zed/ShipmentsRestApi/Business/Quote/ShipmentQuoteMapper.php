1837facaf459fc4c054ff6ac1792d5a897f507ce
Chris Egger <christopher.egger@fondof.de>
2020-02-28 14:08:52 +0100

    Move in right folder

--- src/FondOfSpryker/Zed/ShipmentsRestApi/Business/Quote/ShipmentQuoteMapper.php
+++ src/FondOfSpryker/Zed/ShipmentsRestApi/Business/Quote/ShipmentQuoteMapper.php
@@ -2,13 +2,45 @@
 
 namespace FondOfSpryker\Zed\ShipmentsRestApi\Business\Quote;
 
-use Generated\Shared\Transfer\ExpenseTransfer;
-use Generated\Shared\Transfer\ShipmentMethodTransfer;
-use Spryker\Shared\Shipment\ShipmentConstants;
+use Generated\Shared\Transfer\QuoteTransfer;
+use Generated\Shared\Transfer\RestCheckoutRequestAttributesTransfer;
+use Generated\Shared\Transfer\ShipmentTransfer;
 use Spryker\Zed\ShipmentsRestApi\Business\Quote\ShipmentQuoteMapper as SprykerShipmentQuoteMapper;
 
-class ShipmentQuoteMapper extends SprykerShipmentQuoteMapper
+class ShipmentQuoteMapper extends SprykerShipmentQuoteMapper implements ShipmentQuoteMapperInterface
 {
+    /**
+     * @param \Generated\Shared\Transfer\RestCheckoutRequestAttributesTransfer $restCheckoutRequestAttributesTransfer
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return \Generated\Shared\Transfer\QuoteTransfer
+     */
+    public function mapShipmentToQuoteItem(
+        RestCheckoutRequestAttributesTransfer $restCheckoutRequestAttributesTransfer,
+        QuoteTransfer $quoteTransfer
+    ): QuoteTransfer {
+        $idShipmentMethod = $restCheckoutRequestAttributesTransfer->getShipment()->getIdShipmentMethod();
+        foreach ($quoteTransfer->getItems() as $item) {
+            $shipment = $item->getShipment();
+            if ($shipment === null) {
+                $shipment = new ShipmentTransfer();
+            }
+
+            $shipmentMethodTransfer = $this->shipmentFacade->findAvailableMethodById($idShipmentMethod, $quoteTransfer);
+
+            if ($shipmentMethodTransfer === null) {
+                return $quoteTransfer;
+            }
+
+            $shipment->setMethod($shipmentMethodTransfer)
+                ->setShipmentSelection((string)$idShipmentMethod)
+                ->setShippingAddress(clone $quoteTransfer->getBillingAddress());
+            $item->setShipment($shipment);
+        }
+
+        return $quoteTransfer;
+    }
+
     /**
      * @param \Generated\Shared\Transfer\ShipmentMethodTransfer $shipmentMethodTransfer
      *
